0c28ef3d20008505aaa1fa3f5c59145571e8b8ad
avk393 <ankithk@utexas.edu>
2019-04-07 17:20:30 -0500

    Update PeriodMeasure.c

--- PeriodMeasure.c
+++ PeriodMeasure.c
@@ -2,25 +2,6 @@
 // Runs on LM4F120/TM4C123
 // Use Timer0A in 24-bit edge time mode to request interrupts on the rising
 // edge of PB6 (T0CCP0), and measure period between pulses.
-// Daniel Valvano
-// May 5, 2015
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
-   Example 7.2, Program 7.2
-
- Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 
 // external signal connected to PB6 (T0CCP0) (trigger on rising edge)
 #include <stdint.h>
