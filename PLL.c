157128829dad337c3756781d18ffec42743dc899
avk393 <ankithk@utexas.edu>
2019-04-07 17:20:06 -0500

    Update PLL.c

--- PLL.c
+++ PLL.c
@@ -1,25 +1,6 @@
 // PLL.c
 // Runs on LM4F120/TM4C123
 // A software function to change the bus frequency using the PLL.
-// Daniel Valvano
-// May 2, 2015
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
-   Program 2.10, Figure 2.37
-
- Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
  
 #include <stdint.h>
 #include "PLL.h"
