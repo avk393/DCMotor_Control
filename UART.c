929f810a20fbb9902cdcaa06bc84e02bcbdd85a6
avk393 <ankithk@utexas.edu>
2019-04-07 17:21:42 -0500

    Update UART.c

--- UART.c
+++ UART.c
@@ -2,28 +2,6 @@
 // Runs on LM4F120/TM4C123
 // Use UART0 to implement bidirectional data transfer to and from a
 // computer running PuTTy. Interrupts and software FIFOs are used.
-// Daniel Valvano
-// September 20, 2018
-// Modified by EE345L students Charlie Gough && Matt Hawk
-// Modified by EE345M students Agustinus Darmawan && Mingjie Qiu
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2018
-   Program 5.11 Section 5.6, Program 3.10
-
- Copyright 2018 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
-
 // U0Rx (VCP receive) connected to PA0
 // U0Tx (VCP transmit) connected to PA1
 #include <stdint.h>
